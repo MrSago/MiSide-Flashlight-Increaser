85c7ab608b81db7078cef6307c5de83d453f4476
MrSago <37180898+MrSago@users.noreply.github.com>
2024-12-27 01:12:34 +0300

    fix: link to mod

--- Properties/BuildInfo.cs
+++ Properties/BuildInfo.cs
@@ -8,7 +8,7 @@ internal static class BuildInfo
     public const string COMPANY = null;
     public const string VERSION = "1.0.1";
     public const string GAMEVERSION = null;
-    public const string DOWNLOADLINK = "https://github.com/MrSago/MiSide-FlashlightIncreaser";
+    public const string DOWNLOADLINK = "https://github.com/MrSago/MiSide-Flashlight-Increaser";
     public const string PACKAGE = "com.flashlightincreaser.miside";
     public const string GUID = "87b48efc-7b1f-4d2d-8105-c6fad0116f84";
 }
