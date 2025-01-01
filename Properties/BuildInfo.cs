ec744b1e977e16d29f436814bc86cf897c2d1b0c
MrSago <37180898+MrSago@users.noreply.github.com>
2025-01-01 23:53:48 +0300

    fix: optimize and fixes

--- Properties/BuildInfo.cs
+++ Properties/BuildInfo.cs
@@ -6,9 +6,9 @@ internal static class BuildInfo
     public const string DESCRIPTION = "Increases the range and radius of the flashlight";
     public const string AUTHOR = "MrS4g0";
     public const string COMPANY = null;
-    public const string VERSION = "1.0.3";
+    public const string VERSION = "1.0.4";
     public const string GAMEVERSION = null;
     public const string DOWNLOADLINK = "https://github.com/MrSago/MiSide-Flashlight-Increaser";
-    public const string PACKAGE = "com.flashlightincreaser.miside";
+    public const string PACKAGE = "com.miside.flashlightincreaser";
     public const string GUID = "87b48efc-7b1f-4d2d-8105-c6fad0116f84";
 }
